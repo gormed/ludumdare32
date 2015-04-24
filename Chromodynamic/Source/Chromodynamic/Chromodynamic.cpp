fb87f88d9d21302f7ec4f97ccea779f928447a87
gormed <hans.ferchland@gmx.de>
2015-04-25 01:05:46 +0200

    * movement adjusted * background sprite unification * weapon load collision corrected * stuck protection changes

--- Chromodynamic/Source/Chromodynamic/Chromodynamic.cpp
+++
@@ -1,5 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "Chromodynamic.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Chromodynamic, "Chromodynamic" );
